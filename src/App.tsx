13b8c8e2a6879eab132ba77e92cd024eac73f854
Yumy.Yang <yumy.yang@ehsn.com.tw>
2024-11-07 17:46:54 +0800

    add - input value

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+interface RangeType {
+  width: number;
+  height: number;
+}
+
+type PatternType = "random" | "array" | "spiral" | "concentric";
+
 function App() {
+  const [range, setRange] = useState<RangeType>({
+    width: 100,
+    height: 100,
+  });
+  const [rotate, setRotate] = useState<number>(0);
+  const [space, setSpace] = useState<number>(0);
+  const [pattern, setPattern] = useState<PatternType>("random");
+
   return (
     <div className="flex">
-      <div className="h-screen  shadow-lg">
+      <div className="h-screen shadow-lg w-72">
         <p className="bg-gray-100 p-3 text-center ">點座標生成設置</p>
         <div className="p-5">
           <div className="mb-4">
@@ -13,9 +29,13 @@ function App() {
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value={range.width}
+              onChange={(e) => setRange({ ...range, width: +e.target.value })}
               id="width"
-              type="text"
+              type="number"
               placeholder="width"
+              min="1"
+              max="500"
             />
           </div>
           <div className="mb-4">
@@ -27,9 +47,13 @@ function App() {
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value={range.height}
+              onChange={(e) => setRange({ ...range, height: +e.target.value })}
               id="height"
-              type="text"
+              type="number"
               placeholder="height"
+              min="1"
+              max="500"
             />
           </div>
           <div className="mb-4">
@@ -42,11 +66,13 @@ function App() {
             <select
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="pattern"
+              value={pattern}
+              onChange={(e) => setPattern(e.target.value as PatternType)}
             >
-              <option value="random">random 隨機點</option>
-              <option value="array">array 陣列點</option>
-              <option value="spiral">spiral 螺旋點</option>
-              <option value="concentric">concentric 同心圓點</option>
+              <option value="random">random</option>
+              <option value="array">array</option>
+              <option value="spiral">spiral</option>
+              <option value="concentric">concentric</option>
             </select>
           </div>
           <div className="mb-4">
@@ -61,6 +87,10 @@ function App() {
               id="rotate"
               type="number"
               placeholder="rotate"
+              min="1"
+              max="360"
+              value={rotate}
+              onChange={(e) => setRotate(parseInt(e.target.value, 10))}
             />
           </div>
           <div className="mb-8">
@@ -75,6 +105,10 @@ function App() {
               id="space"
               type="number"
               placeholder="space"
+              min="1"
+              max="500"
+              value={space}
+              onChange={(e) => setSpace(+e.target.value)}
             />
           </div>
           <div className="flex items-center justify-center gap-1">
